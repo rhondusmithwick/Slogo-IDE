395c18ea9a26122304f08b7b36713871a6b0096c
calisnelson <can18@duke.edu>
2016-02-21 23:02:00 -0500

    add internal apis

---
+++ src/view/TurtleAreaInterface.java
@@ -0,0 +1,22 @@
+package view;
+/**
+ * This is an internal API for the view that sets a contract for the Turtles display area. It ensures that any class that creates and manages the turtle display area,
+ * must have a method to set background color and a method to return the node upon which the turtle is drawing.
+ */
+import javafx.scene.Node;
+
+public interface TurtleAreaInterface {
+    /**
+     * Sets the background color of the turtles display area
+     * @param color Color to set the background to
+     */
+    void setBackground(String color);
+    
+    
+    /**
+     * Returns the node that the turtle is drawing on top of. It is only a Node so that the actual object the turtle draw on top of can be anything from a rectangle to
+     * a javafx canvas, to a preset image.
+     * @return Node underlying turtle area
+     */
+    Node getTurtleArea();
+}
