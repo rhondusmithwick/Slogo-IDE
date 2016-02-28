ffe01777cba1cf73df873eb5994057e8520cf6b9
calisnelson <can18@duke.edu>
2016-02-28 14:26:56 -0500

    changes to clickable commands

--- src/view/CommandEntry.java
+++ src/view/CommandEntry.java
@@ -55,7 +55,9 @@ public class CommandEntry implements CommandEntryInterface {
 
     @Override
     public void passInternalCommands(String command) {
-        myEntryBox.setText(command);
+        String curr = myEntryBox.getText();
+        curr = curr + "\n" + command;
+        myEntryBox.setText(curr);
 
     }
 
