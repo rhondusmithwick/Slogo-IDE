fd9a74bbc5b08accf74c7fd3edf891db93eada09
calisnelson <can18@duke.edu>
2016-03-03 21:45:23 -0500

    Revert "rename subpackages"
    This reverts commit a1fb200c22d139f362b2bae5c466c49aa227ba1b.

--- src/Maps/IndexMap.java
+++ src/Maps/IndexMap.java
@@ -2,7 +2,7 @@ package Maps;
 
 import Observables.MapObservable;
 import view.Defaults;
-import view.xml.MapFromXML;
+import view.Xml.MapFromXML;
 
 public abstract class IndexMap {
     protected IndexMap() throws Exception{
