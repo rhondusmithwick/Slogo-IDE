51cb84aea235403a62d41f315b4ae7f3c2c2f21c
calisnelson <can18@duke.edu>
2016-03-07 17:35:21 -0500

    more doc

--- src/view/turtdisplay/TurtleDisplay.java
+++ src/view/turtdisplay/TurtleDisplay.java
@@ -12,6 +12,11 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import observables.ObjectObservable;
 
+/**
+ * Class responsible for containing area where turtle is displayed to the user.
+ * @author Cali
+ *
+ */
 
 public class TurtleDisplay implements Observer {
     private static final double SCROLL_BAR_INITIAL = .5;
@@ -20,6 +25,11 @@ public class TurtleDisplay implements Observer {
     private ScrollPane scroll;
     private ObjectObservable<String> bgColor;
 
+    /**
+     * creates new turtle display instance
+     * @param bgColor string observable that allows background color to be set 
+     * @param turtleDispDimension 2D dimension object that specifies size of turtle area
+     */
     public TurtleDisplay(ObjectObservable<String> bgColor, Dimension2D turtleDispDimension) {
         this.bgColor=bgColor;
         bgColor.addObserver(this);
@@ -47,17 +57,28 @@ public class TurtleDisplay implements Observer {
 
     }
 
-
+    /**
+     * returns scrollpane containing the area turtle can be displayed in
+     * @return scrollpane containg group for turtle to be displayed in
+     */
     public Node getTurtlePane() {
         return scroll;
     }
 
-
+    /**
+     * returns the group contained by scroll pane that turtle and its lines can be added to
+     * @return group turtle and its lines can be added to
+     */
     public Group getTurtleArea() {
         return dispArea;
     }
 
-
+    /**
+     * Called whenever background color string observable is set. This turtle display instance
+     * then changes the background color of the display area to the new color.
+     * @param o object being observed
+     * @param arg1 argument to object
+     */
     @Override
     public void update (Observable o, Object arg1) {
         String color = bgColor.get();
