c609ba5763cf5bfd2c9a4ae91955df18a2841238
calisnelson <can18@duke.edu>
2016-03-07 15:35:49 -0500

    more documentation

--- src/maps/IndexMap.java
+++ src/maps/IndexMap.java
@@ -1,5 +1,11 @@
 package maps;
 
+/**
+ * This class represents a map from an Integer index to a string value.  It is backed by
+ * a MapObservable so that observers can be notified whenever components are added to the map.
+ * @author calinelson
+ */
+
 import observables.MapObservable;
 import view.Defaults;
 import view.xml.MapFromXML;
@@ -9,6 +15,11 @@ public abstract class IndexMap {
         addElements(Defaults.DEFAULT.getDefault());
     }
     
+    /**
+     * Creates a new map and adds elements to the map.
+     * @param type Where the elements are added from. Can be either an XML file name or "Default"
+     * @throws Exception 
+     */
     public void addElements(String type) throws Exception{
         newMap();
         if(type.equals(Defaults.DEFAULT.getDefault())){
@@ -18,22 +29,57 @@ public abstract class IndexMap {
         }
     }
     
+    /**
+     * creates a new mapObservable object
+     */
     protected abstract void newMap ();
 
+    /**
+     * Obtains the elements for a new map from an xml file, or if the type is
+     * "Default" sets default values.
+     * @param type Either an XML file name or "Default"
+     * @throws Exception
+     */
     protected void getElementsFromXML(String type) throws Exception{
         MapFromXML xMap = new MapFromXML(this.getIndexMap());
         String direct = getDirectory();
         xMap.getElements(direct+type);
     }
 
+    /**
+     * Returns the location in which XML files containing saved maps 
+     * are saved 
+     * @return file directory path as string
+     */
     protected abstract String getDirectory ();
 
     
+    /**
+     * If the type is default sets the map values and keys to a set of default entry
+     * values
+     * @throws Exception
+     */
     protected abstract void defaultElements () throws Exception;
 
+    /**
+     * returns the MapObservable object that backs the index map
+     * @return MapObservable backing index map
+     */
     public abstract MapObservable<Integer, String> getIndexMap();
+    
+    /**
+     * adds a key, value pair to the index map
+     * @param index index to set value at
+     * @param value value to be set for the index
+     * @throws Exception
+     */
     public abstract void setAtIndex(int index, String value) throws Exception;
     
+    /**
+     * returns a value string based on the given index key
+     * @param key index to get string at
+     * @return value string associated with key index
+     */
     public abstract String get(int key);
 
 }
