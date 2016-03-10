b475735dca32ee86c26f3f0b89f63f45634fdb22
Jonathan Ma <jonathan.ma@duke.edu>
2016-03-10 16:40:43 -0500

    refactored turtleboolean to follow function passing

--- src/model/turtleboolean/TurtleBoolean.java
+++ src/model/turtleboolean/TurtleBoolean.java
@@ -1,11 +1,17 @@
 package model.turtleboolean;
 
+import java.util.function.DoubleBinaryOperator;
+
 import model.treenode.CommandNode;
 
 public abstract class TurtleBoolean extends CommandNode {
-
-    protected abstract double conditional();
-
+	
+	private final DoubleBinaryOperator func;
+	
+	protected TurtleBoolean(DoubleBinaryOperator func) {
+		this.func = func;
+	}
+	
     @Override
     protected int getNumChildrenRequired() {
         return 2;
@@ -13,6 +19,8 @@ public abstract class TurtleBoolean extends CommandNode {
 
     @Override
     protected double execute() {
-        return conditional();
+        double value1 = getChildren().get(0).getValue();
+        double value2 = getChildren().get(1).getValue();
+        return func.applyAsDouble(value1, value2);
     }
 }
