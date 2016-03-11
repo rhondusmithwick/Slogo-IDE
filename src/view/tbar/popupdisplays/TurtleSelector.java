01e0a8571b0d6fddf5d4c0ba387ec420280e7e35
Stephen Kwok <smk44@duke.edu>
2016-03-11 18:14:58 -0500

    Started editing TurtleSelector to hide turtles

--- src/view/tbar/popupdisplays/TurtleSelector.java
+++ src/view/tbar/popupdisplays/TurtleSelector.java
@@ -1,5 +1,9 @@
 package view.tbar.popupdisplays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Label;
 import observables.ObjectObservable;
@@ -14,6 +18,8 @@ import observables.ObjectObservable;
 
 public class TurtleSelector extends TurtlePropertyUpdater {
 	
+	private SimpleStringProperty turtleIDs;
+	
 	/**
 	 * creates a new turtleselector instance
 	 * @param turtleIDs SimpleStringProperty list of turtle ids
@@ -23,6 +29,7 @@ public class TurtleSelector extends TurtlePropertyUpdater {
 	public TurtleSelector(SimpleStringProperty turtleIDs,
 			ObjectObservable<String> internalCommand, ObjectObservable<String> parsingLanguage) {
 		super(turtleIDs, internalCommand, parsingLanguage, "turtleSelectTitle");
+		this.turtleIDs=turtleIDs;
 	}
 
 	@Override
@@ -33,7 +40,22 @@ public class TurtleSelector extends TurtlePropertyUpdater {
 	@Override
 	protected String makeCommand(String turtleIDs) {
 		String tellCommand = translateCommand("Tell");
-		return tellCommand + " [ " + turtleIDs + "]";
+		String askCommand = translateCommand("Ask");
+		String hideCommand = translateCommand("HideTurtle");
+		String inactive = getInactive(turtleIDs);
+		String comm = tellCommand + " [ " + turtleIDs + "]\n" + askCommand + " [" + inactive + "] [ " + hideCommand + " ]";
+		System.out.println(comm);
+		return comm;
+	}
+
+	private String getInactive(String turtles) {
+		ArrayList<String> active = new ArrayList<String>( Arrays.asList(turtles.split(" ")));
+		ArrayList<String> allTurtles = new ArrayList<String> (Arrays.asList(turtleIDs.get().split(", ")));
+		Predicate<String> isIn = (e) -> (active.contains(e));
+		Object[] inActive =   allTurtles.parallelStream().filter(isIn).toArray();
+		StringBuilder toHide = new StringBuilder();
+		Arrays.asList(inActive).stream().forEach(e-> toHide.append(((String) e) + " "));
+		return toHide.toString();
 	}
 
 }
