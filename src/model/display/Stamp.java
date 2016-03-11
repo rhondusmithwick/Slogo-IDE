c14d4994824111f216e4550366e3da864fee52f8
Jonathan Ma <jonathan.ma@duke.edu>
2016-03-10 20:56:06 -0500

    added basic shell for stamp

--- src/model/display/Stamp.java
+++ src/model/display/Stamp.java
@@ -1,5 +1,14 @@
 package model.display;
 
-public class Stamp {
+import model.treenode.TurtleCommandNode;
+import model.turtle.Turtle;
+
+public class Stamp extends TurtleCommandNode {
+	
+	@Override
+	public double turtleExecute(Turtle turtle) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
