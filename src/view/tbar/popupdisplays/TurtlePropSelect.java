63e51e07aad7be7cabbfd426ec9881654c9e3138
calisnelson <can18@duke.edu>
2016-03-11 16:01:36 -0500

    set up properties

--- src/view/tbar/popupdisplays/TurtlePropSelect.java
+++ src/view/tbar/popupdisplays/TurtlePropSelect.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import observables.ObjectObservable;
 import view.Defaults;
 import view.Size;
@@ -19,21 +20,26 @@ public class TurtlePropSelect extends PopUp {
 	private ComboBox<String> turtleSelect;
 
 	public TurtlePropSelect(ObjectObservable<Integer> selectedTurtle, SimpleStringProperty turtleIds){
-		super(Size.TURTLE_UPDATE_POPUP_HEIGHT.getSize(), Size.TURTLE_UPDATE_POPUP_WIDTH.getSize(), 
+		super(Size.TURTLE_UPDATE_POPUP_WIDTH.getSize(), Size.PROP_SEL_HEIGHT.getSize(), 
 				Defaults.BACKGROUND_COLOR.getDefault());
 		this.myResources = ResourceBundle.getBundle(Defaults.DISPLAY_LOC.getDefault());
+		this.selectedTurtle=selectedTurtle;
+		this.turtleIds=turtleIds;
 	}
 	
 	@Override
 	protected void createScene() {
+		String IDS = turtleIds.get().substring(1, turtleIds.get().length()-1);
+		Label title = new Label(myResources.getString("turtleParamSel"));
 		turtleSelect = ComboFactory.createBox(myResources.getString("turtleParamSel"), 
-				Arrays.asList(turtleIds.get().split(",")), e->updateSelected());
-		addNodes(Arrays.asList(turtleSelect));
+				Arrays.asList(IDS.split(", ")), e->updateSelected());
+		addNodes(Arrays.asList(title, turtleSelect));
 
 	}
 
 	private void updateSelected() {
 		selectedTurtle.set(Integer.parseInt(turtleSelect.getSelectionModel().getSelectedItem()));
+		closeScene();
 	}
 
 }
