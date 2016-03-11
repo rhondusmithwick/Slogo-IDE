99b044c4ef4b3388a552c4f7ec27b5d7abe4d034
Rhondu Smithwick <rhondusmithwick@gmail.com>
2016-03-10 20:30:35 -0500

    Added turtle defaults. Unbound line stroke property in movement at end.

---
+++ src/model/turtle/TurtleDefaults.java
@@ -0,0 +1,34 @@
+package model.turtle;
+
+/**
+ * Created by rhondusmithwick on 3/10/16.
+ *
+ * @author Rhondu Smithwick
+ */
+public enum TurtleDefaults {
+    TURTLE_IMAGE("resources/images/blackarrow.png"),
+    VISIBLE("true"),
+    HEADING("180"),
+    PEN_DOWN("true"),
+    PEN_SIZE("1"),
+    PEN_COLOR("black");
+
+    private String content;
+
+    TurtleDefaults(String content) {
+        this.content = content;
+    }
+
+
+    public String getString() {
+        return content;
+    }
+
+    public Double getDouble() {
+        return Double.valueOf(content);
+    }
+
+    public Boolean getBoolean() {
+        return Boolean.valueOf(content);
+    }
+}
