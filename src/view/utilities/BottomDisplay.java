f98d2dc96d5e2bb9288402555f10389abb2fde6d
calisnelson <can18@duke.edu>
2016-03-12 01:09:40 -0500

    add comments

--- src/view/utilities/BottomDisplay.java
+++ src/view/utilities/BottomDisplay.java
@@ -5,8 +5,18 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import view.Size;
 
+/**
+ * Abstract class acts as a framework for displays on the bottom part of the border pane
+ * @author Cali
+ *
+ */
+
 public abstract class BottomDisplay {
 
+    /**
+     * creates scrollpanes for the displays in the bottom part of the border pane
+     * @return created scrollpane
+     */
     protected ScrollPane setScroll(){
         ScrollPane myScrollPane = new ScrollPane();
         myScrollPane.setMinViewportHeight(Size.BOTTOM_HEIGHT.getSize());
