4a7efac821f76d1cda865a15e9c9549294c7019f
calisnelson <can18@duke.edu>
2016-03-11 21:25:26 -0500

    edit turtle selector

--- src/view/tbar/popupdisplays/TurtleSelector.java
+++ src/view/tbar/popupdisplays/TurtleSelector.java
@@ -17,47 +17,70 @@ import observables.ObjectObservable;
  */
 
 public class TurtleSelector extends TurtlePropertyUpdater {
-	
-	private static final int END_STRING = 2;
-	private SimpleStringProperty turtleIDs;
-	
-	/**
-	 * creates a new turtleselector instance
-	 * @param turtleIDs SimpleStringProperty list of turtle ids
-	 * @param internalCommand object observable used to pass commands to command entry 
-	 * @param parsingLanguage simplestringproperty of current parsing language
-	 */
-	public TurtleSelector(SimpleStringProperty turtleIDs,
-			ObjectObservable<String> internalCommand, ObjectObservable<String> parsingLanguage) {
-		super(turtleIDs, internalCommand, parsingLanguage, "turtleSelectTitle");
-		this.turtleIDs=turtleIDs;
-	}
 
-	@Override
-	protected void createElementsBelowCheckBoxes() {
-		addToScene(new Label(getStringFromResources("turtleSelectClickButtonPrompt")));
-	}
+    private static final int END_STRING = 2;
+    private SimpleStringProperty turtleIDs;
 
-	@Override
-	protected String makeCommand(String turtleIDs) {
-		String tellCommand = translateCommand("Tell");
-		String askCommand = translateCommand("Ask");
-		String hideCommand = translateCommand("HideTurtle");
-		String inactive = getInactive(turtleIDs);
-		return tellCommand + " [ " + turtleIDs + "]\n" + askCommand 
-				+ " [ " + inactive + " ] [ " + hideCommand + " ]\n" + translateCommand("ShowTurtle");
-		
+    /**
+     * creates a new turtleselector instance
+     * @param turtleIDs SimpleStringProperty list of turtle ids
+     * @param internalCommand object observable used to pass commands to command entry 
+     * @param parsingLanguage simplestringproperty of current parsing language
+     */
+    public TurtleSelector(SimpleStringProperty turtleIDs,
+                          ObjectObservable<String> internalCommand, ObjectObservable<String> parsingLanguage) {
+        super(turtleIDs, internalCommand, parsingLanguage, "turtleSelectTitle");
+        this.turtleIDs=turtleIDs;
+    }
 
-	}
+    @Override
+    protected void createElementsBelowCheckBoxes() {
+        addToScene(new Label(getStringFromResources("turtleSelectClickButtonPrompt")));
+    }
 
-	private String getInactive(String turtles) {
-		List<String> active =  Arrays.asList(turtles.split(" "));
-		List<String> allTurtles =  Arrays.asList(turtleIDs.get().split(", "));
-		Predicate<String> isIn = (e) -> (!active.contains(e));
-		Object[] inActive =   allTurtles.parallelStream().filter(isIn).toArray();
-		StringBuilder toHide = new StringBuilder();
-		Arrays.asList(inActive).stream().forEach(e-> toHide.append(((String) e) + " "));
-		return toHide.toString().substring(1, toHide.length()-END_STRING);
-	}
+    @Override
+    protected String makeCommand(String turtleIDs) {
+        String tellCommand = getTellCommand(turtleIDs);
+        String inactive = getInactive(turtleIDs);
+        String askCommand = getAskCommand(inactive);
+        String comm= tellCommand +  askCommand + translateCommand("ShowTurtle");
+        System.out.println(comm);
+        return comm;
+
+
+    }
+
+    private String getAskCommand (String inactive) {
+        String askCommand;
+        if(inactive.length()==0){
+            askCommand = "";
+        }else{
+            askCommand = translateCommand("Ask") + " [ " + inactive + " ] [ " + translateCommand("HideTurtle") + " ]\n";
+        }
+        return askCommand;
+    }
+
+    private String getTellCommand (String turtleIDs) {
+        String tellCommand;
+        if(turtleIDs.length()==0){
+            tellCommand = "";
+        }else{
+            tellCommand = translateCommand("Tell") + " [ " + turtleIDs + "]\n";
+        }
+        return tellCommand;
+    }
+
+    private String getInactive(String turtles) {
+        List<String> active =  Arrays.asList(turtles.split(" "));
+        List<String> allTurtles =  Arrays.asList(turtleIDs.get().split(", "));
+        if(active.size() == allTurtles.size()){
+            return "";
+        }
+        Predicate<String> isIn = (e) -> (!active.contains(e));
+        Object[] inActive =   allTurtles.parallelStream().filter(isIn).toArray();
+        StringBuilder toHide = new StringBuilder();
+        Arrays.asList(inActive).stream().forEach(e-> toHide.append(((String) e) + " "));
+        return toHide.toString().substring(1, toHide.length()-END_STRING);
+    }
 
 }
